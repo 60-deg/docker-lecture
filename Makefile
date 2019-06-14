addce4f8e65ccc15f6b195d2780110b962a53290
60° <hatsumi.yamane.4609@gmail.com>
2019-06-15 05:26:53 +0900

    pisconのアプリケーションをdockerで動かす

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: dev
 dev:
 	docker-compose -f development/docker-compose.yaml up --build
+
+.PHONY: iscogram
+iscogram:
+	docker-compose -f iscogram/docker-compose.yaml up --build
---
+++ iscogram/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+go get "github.com/bradfitz/gomemcache/memcache"
+go get "github.com/bradleypeabody/gorilla-sessions-memcache"
+go get "github.com/go-sql-driver/mysql"
+go get "github.com/gorilla/sessions"
+go get "github.com/jmoiron/sqlx"
+go get "github.com/zenazn/goji"
